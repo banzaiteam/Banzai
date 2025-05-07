a5b988a695cbf6df45abbf08cef5a222bbec519c
sergei00026 <sergei57fx@mail.ru>
2025-05-07 17:03:55 +0300

    BAN-7 init: Textarea-task

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
       Banzai
+      <p>some text</p>
             <Icon name="home-outline" size={92} stroke='#fff'  />
     </main>
   );
