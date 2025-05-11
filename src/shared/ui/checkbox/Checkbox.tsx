f1993b33e0fb80a37ab796fefc76e68050736aef
Kkatsiarryna <katepicastor@gmail.com>
2025-05-11 22:55:38 +0300

    Checkbox component

--- src/shared/ui/checkbox/Checkbox.tsx
+++ src/shared/ui/checkbox/Checkbox.tsx
@@ -1,6 +1,21 @@
-export const Checkbox = () => {
-    return (
-        <>
-        </>
-    )
-}
+import * as CheckboxRadix from "@radix-ui/react-checkbox";
+import { CheckIcon } from "@radix-ui/react-icons";
+import s from "./Checkbox.module.css";
+import {ComponentPropsWithoutRef} from "react";
+
+type Props =   {
+    label? : string
+} & ComponentPropsWithoutRef<typeof CheckboxRadix.Root>
+
+export const Checkbox= ({ label, ...rest}: Props) => (
+    <div className={s.checkbox }>
+        <CheckboxRadix.Root className={s.Root} {...rest}>
+            <CheckboxRadix.Indicator >
+                <CheckIcon />
+            </CheckboxRadix.Indicator>
+        </CheckboxRadix.Root>
+        {label && <label className={s.Label}>
+            {label}
+        </label>}
+    </div>
+)
