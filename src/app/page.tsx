6ce8d84d96367a798e5f8045df95e4f0f237d58b
Andrew <dracon.24041230@mail.ru>
2025-05-13 23:47:30 +0300

    BAN-5 refactor: add Input component in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,19 @@
 import colors from "tailwindcss/colors";
 import {Icon} from "@shared/ui/icon/Icon";
-import {RadixTextarea} from "@shared/ui";
+import {Input, RadixTextarea} from "@shared/ui";
+
 
 export default function Page() {
   return (
-    <main >
-      Banzai
-      <RadixTextarea/>
-      <RadixTextarea disabled/>
-      <RadixTextarea error errorMessage='Ошибка'/>
-            <Icon name="home-outline" size={92} stroke='#fff'  />
-    </main>
+      <main>
+          Banzai
+          <Input  placeholder={'text...'}/>
+          <Input disabled placeholder={'text...'}/>
+          <Input placeholder={'text...'} error helperText={'some error...'}/>
+          <RadixTextarea/>
+          <RadixTextarea disabled/>
+          <RadixTextarea error errorMessage='Ошибка'/>
+                <Icon name="home-outline" size={92} stroke='#fff'/>
+      </main>
   );
 }
