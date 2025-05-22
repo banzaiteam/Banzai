9bf0ab92132def4d10f0f6b742e275eb142e13f1
sergei00026 <sergei57fx@mail.ru>
2025-05-22 15:50:15 +0300

    BAN-9-Cards init: card

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -2,4 +2,5 @@ export { Alert } from "./alert/Alert";
 export {Textarea} from './textarea/Textarea'
 export {Pagination} from './pagination/Pagination'
 export {Scroll} from './scroll/Scroll'
+export {Card} from './card/Card'
 
