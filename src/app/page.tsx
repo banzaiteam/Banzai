fee6f6afbcfefe819c3ffc548f5dbc440dabb6d4
sergei00026 <sergei57fx@mail.ru>
2025-05-22 15:05:52 +0300

    fix: clear page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,9 @@
 'use client'
-import {Pagination, Textarea} from "@shared/ui";
-import { Calendar } from "@/assets/icons/components";
-import {useState} from "react";
 
 export default function Page() {
   return (
     <main >
       Banzai
-      <Textarea title={'Заголовок'} placeholder="Введите текст..."/>
-      <Textarea title={'Заголовок'} placeholder="Введите текст..." disabled/>
-      <Textarea title={'Заголовок'} errorMessage='Ошибка'/>
-      <Calendar/>
-      <div style={{padding: '40px'}}>
-        <Pagination/>
-      </div>
-
     </main>
   );
 }
