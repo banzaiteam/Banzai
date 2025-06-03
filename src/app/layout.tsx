2d679d4c62ce87dcbfda8a515b27bfca4e9afaf1
Andrew <dracon.24041230@mail.ru>
2025-06-03 22:28:03 +0300

    BAN-27 fix: layout page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-import Page from "@/app/page";
 import '@shared/styles/global.css' // Подключение глобальных стилей
 import '@radix-ui/themes/styles.css';
 import {Scroll} from "@shared/ui";
@@ -14,7 +13,7 @@ export default function RootLayout({
           <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
     <body>
     <Scroll >
-      <Page/>
+      {children}
     </Scroll>
     </body>
     </html>
