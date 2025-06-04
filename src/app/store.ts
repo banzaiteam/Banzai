bd240655a0423f4b1dacf941871bb72df57fa0e7
Andrew <dracon.24041230@mail.ru>
2025-06-04 21:04:31 +0300

    BAN-27 add: add appSlice in the store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,13 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { baseApi } from '@/shared/api/baseApi';
+import {appSlice} from "@shared/store/slices/appSlice";
 
 export const store = configureStore({
   reducer: {
     [baseApi.reducerPath]: baseApi.reducer,
+    [appSlice.reducerPath]: appSlice.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(baseApi.middleware),
 });
+/*@ts-ignore*/
+window.getState = store.getState;
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
