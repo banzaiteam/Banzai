cb1ddef089f883e359919db4512774d1b03cebce
Andrew <dracon.24041230@mail.ru>
2025-06-06 19:21:19 +0300

    BAN-27 add: add error Handle

--- src/shared/api/baseApi.ts
+++ src/shared/api/baseApi.ts
@@ -1,19 +1,24 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import {handleError} from "@shared/utils/handleError";
 
 export const baseApi = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({
-    baseUrl: 'https://gate.yogram.ru/api/v1',
-    prepareHeaders: (headers) => {
-      // Получаем токен из localStorage
-      const token = localStorage.getItem('access_token');
-      // Если токен есть, добавляем его в заголовки
-      if (token) {
-        headers.set('authorization', `Bearer ${token}`);
-      }
-      return headers;
-    },
-  }),
+  baseQuery: async (args, api, extraOptions) => {
+    const result = await fetchBaseQuery({
+      baseUrl: 'https://gate.yogram.ru/api/v1',
+      prepareHeaders: (headers) => {
+        // Получаем токен из localStorage
+        const token = localStorage.getItem('access_token');
+        // Если токен есть, добавляем его в заголовки
+        if (token) {
+          headers.set('authorization', `Bearer ${token}`);
+        }
+        return headers;
+      },
+    })(args, api, extraOptions);
+    handleError(api,result);
+    return result
+  },
   endpoints: () => ({}),
   tagTypes: ['User', 'Post'],
 })
