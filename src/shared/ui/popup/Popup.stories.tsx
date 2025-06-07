0bfc93191c757baa109f980905959d13afad03cb
Andrew <dracon.24041230@mail.ru>
2025-06-07 17:57:45 +0300

    BAN-27 init: Popup

---
+++ src/shared/ui/popup/Popup.stories.tsx
@@ -0,0 +1,21 @@
+
+import type { Meta, StoryObj } from '@storybook/react'
+import { Popup } from '@/shared/ui'
+
+
+const meta = {
+  title: 'Components/Popup',
+  component: Popup,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {},
+} satisfies Meta<typeof Popup>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+   args: {},
+}
