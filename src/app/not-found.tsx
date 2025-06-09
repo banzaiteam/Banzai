02c2500dff71ded2806647ce05eb5e0ea05a090b
sergei00026 <sergei57fx@mail.ru>
2025-06-09 08:17:30 +0300

    BAN-28-Log-In init: fix bags

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,15 +1,13 @@
 'use client';
 import {Typography} from "@shared/ui";
 
-type Props = {
-
-};
- const NotFound = (props: Props) => {
+type Props = {};
+const NotFound = (props: Props) => {
   return (
     <div>
-<Typography variant="h1">404 - Not Found</Typography>
+      <Typography variant="h1">404 - Not Found</Typography>
     </div>
   );
 };
 
- export default NotFound
+export default NotFound
--- src/features/auth/login/model/loginSchema.ts
+++ src/features/auth/login/model/loginSchema.ts
@@ -1,8 +1,19 @@
-import { z } from 'zod';
+import {z} from 'zod';
 
 export const loginSchema = z.object({
-  email: z.string().email('Invalid email address'),
-  password: z.string().min(6, 'Password must be at least 6 characters'),
+  email: z.string()
+    .min(4, 'Email must be at least 4 characters long.')
+    .email('Invalid email address')
+    .trim(),
+  password: z.string()
+    .min(6, 'Password must be at least 6 characters')
+    .max(16, 'Password must be at most 16 characters')
+    .regex(/[0-9]/, {message: 'Contain at least one number.'})
+    .regex(/[^a-zA-Z0-9]/, {
+      message: 'Contain at least one special character.',
+    })
+    .trim(),
 });
 
 export type LoginFormValues = z.infer<typeof loginSchema>;
+
