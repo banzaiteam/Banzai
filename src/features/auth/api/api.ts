96110dd65b459c00d0ebed28d7eeb4d83968964e
Andrew <dracon.24041230@mail.ru>
2025-06-18 11:06:12 +0300

    BAN-27 refactor: refactor structure

--- src/features/auth/api/api.ts
+++ src/features/auth/api/api.ts
@@ -1,5 +1,5 @@
 import { baseApi } from '@shared/api/baseApi'
-import type { AuthResponse } from '../types'
+import type { AuthResponse } from '../model/types'
 
 export const authApi = baseApi.injectEndpoints({
   endpoints: (build) => ({
