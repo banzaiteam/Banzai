701ce3bc242d7dd71ed7acaf473e6dc60ffa25ce
Andrew <dracon.24041230@mail.ru>
2025-06-24 16:57:16 +0300

    BAN-27 fix: signup.api type

--- src/features/auth/model/types/types.ts
+++ src/features/auth/model/types/types.ts
@@ -1,29 +1,28 @@
-
 // Тип для успешного ответа авторизации
 export type AuthResponse = {
-  token: string;
-  refreshToken: string;
+  token: string
+  refreshToken: string
   user: {
-    id: string;
-    email: string;
-    name: string;
-    role: 'user' | 'admin';
-  };
-  expiresIn: number; // Время жизни токена в секундах
-};
+    id: string
+    email: string
+    name: string
+    role: 'user' | 'admin'
+  }
+  expiresIn: number // Время жизни токена в секундах
+}
 
 // Тип для данных входа
 export type LoginCredentials = {
-  email: string;
-  password: string;
-};
+  email: string
+  password: string
+}
 
 // Тип для ошибки API
 export type AuthError = {
-  status: number;
-  message: string;
+  status: number
+  message: string
   errors?: Array<{
-    field: string;
-    message: string;
-  }>;
-};
+    field: string
+    message: string
+  }>
+}
--- src/features/auth/signUp/api/signUp.api.ts
+++ src/features/auth/signUp/api/signUp.api.ts
@@ -1,6 +1,7 @@
 import { baseApi } from '@shared/api/baseApi'
 import { clearAppError } from '@shared/store/slices/appSlice'
 import type { DataSignUp, ResponseSignUp } from '@features/auth/signUp/model/types'
+import type { VerifyEmailData } from '@features/auth/emailVerify/model/types/type'
 
 export const signUpApi = baseApi.injectEndpoints({
   endpoints: build => ({
@@ -20,7 +21,7 @@ export const signUpApi = baseApi.injectEndpoints({
         } catch (error: any) {}
       },
     }),
-    sendVerifyEmail: build.mutation<ResponseSignUp, Pick<DataSignUp, 'email'>>({
+    sendVerifyEmail: build.mutation<ResponseSignUp, VerifyEmailData>({
       query: data => ({
         url: '/signup/send-verify-email',
         method: 'POST',
