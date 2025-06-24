32daf7dcab27e15f2ea2da5cd01927feb83e0043
Andrew <dracon.24041230@mail.ru>
2025-06-24 13:04:39 +0300

    BAN-27 refactor: improve style for Input

--- src/features/auth/components/inputPassword/InputPassword.tsx
+++ src/features/auth/components/inputPassword/InputPassword.tsx
@@ -1,22 +1,30 @@
 'use client'
-import React, {useState} from 'react';
-import {Input, type InputProps, InputSlot} from "@shared/ui";
-import {EyeOffOutline, EyeOutline} from "@/assets/icons/components";
+import React, { useState } from 'react'
+import { Input, type InputProps, InputSlot } from '@shared/ui'
+import { EyeOffOutline, EyeOutline } from '@/assets/icons/components'
 
+export const InputPassword = (props: InputProps) => {
+  const { disabled } = props
+  const [isShowPassword, setIsShowPassword] = useState(false)
+  const onClickHandler = () => {
+    setIsShowPassword(prev => !prev)
+  }
 
-export const InputPassword = (props:InputProps) => {
-
-    const [isShowPassword, setIsShowPassword] = useState(false)
-    const onClickHandler = () => {
-        setIsShowPassword((prev) => !prev)
-    }
-
-    return <Input type={isShowPassword ? 'text' : 'password'} subTitle={'Password'} placeholder={'******************'} aria-required="true" {...props}>
-        <InputSlot onClick={onClickHandler} aria-label={isShowPassword ? "Hide password" : "Show password"}>
-            {isShowPassword ? <EyeOutline/> : <EyeOffOutline/>}
-        </InputSlot>
+  return (
+    <Input
+      type={isShowPassword ? 'text' : 'password'}
+      subTitle={'Password'}
+      placeholder={'******************'}
+      aria-required="true"
+      {...props}
+    >
+      <InputSlot
+        onClick={onClickHandler}
+        aria-label={isShowPassword ? 'Hide password' : 'Show password'}
+        disabled={disabled}
+      >
+        {isShowPassword ? <EyeOutline /> : <EyeOffOutline />}
+      </InputSlot>
     </Input>
-};
-
-
-
+  )
+}
