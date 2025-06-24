bad21d8926440e12c41015040cdc0beaa130e0f8
Andrew <dracon.24041230@mail.ru>
2025-06-24 13:03:55 +0300

    BAN-27 refactor: change json data type from raw into form-data for signUp

--- src/features/auth/signUp/model/types.ts
+++ src/features/auth/signUp/model/types.ts
@@ -2,10 +2,18 @@ export type ResponseSignUp = {
   status: 201 | 409
 }
 
-export type DataSignUp = {
+/*export type DataSignUp = {
   username: string
   email: string
   password: string
   aboutMe?: string
   file?: string
+}*/
+
+export type DataSignUp = FormData & {
+  get(name: 'username'): string
+  get(name: 'email'): string
+  get(name: 'password'): string
+  get(name: 'aboutMe'): string | null
+  get(name: 'file'): File | null
 }
