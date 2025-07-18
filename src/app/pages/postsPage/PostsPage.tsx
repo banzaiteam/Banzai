a527f612d49f1531aefae56c2525ce603fe33e61
Andrew <dracon.24041230@mail.ru>
2025-07-18 12:50:57 +0300

    BAN-71-UC-3 refactor: add control modalShowPost

--- src/app/pages/postsPage/PostsPage.tsx
+++ src/app/pages/postsPage/PostsPage.tsx
@@ -1,14 +1,16 @@
 'use client'
 import { Typography } from '@shared/ui'
 import { ShowPost } from '@features/showPost/ui/ShowPost'
+import { useState } from 'react'
 
 export const PostsPage = () => {
+  const [isOpen, setOpen] = useState(true)
   return (
     <>
       <Typography variant={'h2'} as={'h2'}>
         Posts
       </Typography>
-      <ShowPost open={true} onClose={() => {}} />
+      <ShowPost id={'1718761e-fb9e-4d07-b1a6-5147516bce99'} open={isOpen} onClose={setOpen} />
     </>
   )
 }
