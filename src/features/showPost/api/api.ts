6b7cafe9656ac882696720035619696d9954a163
Andrew <dracon.24041230@mail.ru>
2025-07-18 12:50:05 +0300

    BAN-71-UC-3 refactor: add type for responses

--- src/features/showPost/api/api.ts
+++ src/features/showPost/api/api.ts
@@ -1,17 +1,51 @@
 import { baseApi } from '@shared/api/baseApi'
 /*import type { AuthResponse } from '../model/types/types'*/
-
-type AuthResponse = {}
-
+type Status = 'pending' | 'success'
+type FileData = {
+  id: string
+  createdAt: Date
+  updatedAt: Date
+  fileName: string
+  url: string
+  metatype: string
+  status: Status
+  postId: string
+}
+type PostData = {
+  id: string
+  userId: string
+  isPublished: boolean
+  description: string
+  createdAt: Date
+  updatedAt: Date
+  files: FileData[]
+  comments: object ///🤔
+}
+type PostDataResponse = {
+  items: PostData[]
+  limit: number
+  page: number
+  totalItems: number
+}
+type DeletePostResponse = {}
 export const showPostApi = baseApi.injectEndpoints({
   endpoints: build => ({
-    deletePost: build.mutation<AuthResponse, { id: string }>({
+    deletePost: build.mutation<DeletePostResponse, { id: string }>({
       query: ({ id }: { id: string }) => ({
         url: `/posts/${id}`,
         method: 'DELETE',
       }),
     }),
+    getPostData: build.query<PostDataResponse, string>({
+      query: postid => ({
+        url: `/posts`,
+        method: 'GET',
+        params: {
+          filter: `id:eq:${postid}`,
+        },
+      }),
+    }),
   }),
 })
 /*TODO*/
-export const { useDeletePostMutation } = showPostApi
+export const { useDeletePostMutation, useGetPostDataQuery } = showPostApi
