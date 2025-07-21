527e5008cdf97a90f66d01e1f480831346aa40f0
Andrew <dracon.24041230@mail.ru>
2025-07-21 16:53:07 +0300

    BAN-71-UC-3 refactor: add new tests for signUp

--- cypress/e2e/signUp.cy.ts
+++ cypress/e2e/signUp.cy.ts
@@ -1,7 +1,37 @@
 describe('signUp tests', () => {
-  it('passes', () => {
+  beforeEach(() => {
+    // Замените URL на реальный путь к странице регистрации
     cy.visit('/signup')
     cy.request('/signup')
+  })
+  /// <reference types="cypress" />
+  it('renders all inputs and buttons', () => {
+    cy.get('[data-id="username-field"]').should('exist')
+    cy.get('[data-id="email-field"]').should('exist')
+    cy.get('[data-id="password-field"]').should('exist')
+    cy.get('[data-id="confirm-password-field"]').should('exist')
+    cy.get('[data-id="agreement"]').should('exist')
+    cy.get('[data-id="signup-btn"]').should('exist')
+    cy.contains('Sign Up')
+    cy.contains('Sign In')
+  })
+
+  it('navigates to Sign In page when clicking Sign In link', () => {
+    cy.contains('Sign In').click()
+    cy.url().should('include', '/auth/signIn')
+  })
+
+  it('calls onClickHandler on GitHub button click (alerts)', () => {
+    // Чтобы проверить alert, нужно stub window.alert
+    cy.window().then(win => {
+      cy.stub(win, 'alert').as('alertStub')
+    })
+
+    cy.get('button[aria-label="Sign up with GitHub"]').click()
+    cy.get('@alertStub').should('have.been.calledWith', 'Нажмал')
+  })
+
+  it('passes', () => {
     cy.get('[data-id="username-field"]').type('Andrew')
     cy.get('[data-id="email-field"]').type('andrew@mail.ru')
     cy.get('[data-id="password-field"]').type('asdasdA!1')
