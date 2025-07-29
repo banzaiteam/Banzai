f95b18709a219db9d7601d99793a7c3e734594c0
Kkatsiarryna <katepicastor@gmail.com>
2025-07-29 15:34:17 +0300

    BAN-59 add edit post api

--- src/features/edit-post/ui/EditPostForm.tsx
+++ src/features/edit-post/ui/EditPostForm.tsx
@@ -1,28 +1,41 @@
 import { Button, Popup, Textarea, Typography } from '@shared/ui'
 import { DialogClose } from '@radix-ui/react-dialog'
 import { Close } from '@/assets/icons/components'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import s from './EditPostForm.module.scss'
 import Image from 'next/image'
 import Palm from '@/assets/images/Palm.png'
 import { CircleImage } from '@shared/ui/circleImage/ui/CircleImage'
 import user from '@/assets/images/User.png'
 import { useGetMeQuery } from '@shared/api/userApi'
+import { useGetPostDataQuery } from '@features/showPost/api/api'
+import { useEditPostMutation } from '@features/edit-post/api/editPostApi'
 
 type EditPostForm = {
   open: boolean
   onClose: (value: boolean) => void
+  postId: string
 }
 
 export const EditPostForm = (props: EditPostForm) => {
-  const { onClose, ...rest } = props
-  const [description, setDescription] = useState(
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. кееееееееееееееееееееееееееееееееееееееееееееееееееееееееееееееееееееееееееееееееррррррррррррррррррррррррррррррррррррррррррррррррррррррррр'
-  )
+  const { onClose, postId, ...rest } = props
+  const [description, setDescription] = useState('')
   const maxChars = 500
 
-  const { data } = useGetMeQuery()
-  const username = data?.username
+  const { data: userData } = useGetMeQuery()
+  const username = userData?.profile.username
+
+  const { data: postData } = useGetPostDataQuery(postId)
+  useEffect(() => {
+    if (postData?.items?.[0]?.description) {
+      setDescription(postData?.items?.[0]?.description)
+    }
+  }, [postData])
+
+  const [editPost] = useEditPostMutation()
+  const buttonHandler = () => {
+    editPost({ id: postId, description })
+  }
 
   return (
     <Popup onOpenChange={onClose} size={'xl'} {...rest}>
@@ -48,8 +61,8 @@ export const EditPostForm = (props: EditPostForm) => {
             <Textarea
               className={s.textarea}
               title={'Add publication descriptions'}
-              //defaultValue={description}
-              placeholder={description}
+              defaultValue={description}
+              //placeholder={description}
               onChange={e => setDescription(e.target.value)}
               maxLength={maxChars}
             />
@@ -58,7 +71,7 @@ export const EditPostForm = (props: EditPostForm) => {
             </Typography>
           </div>
           <div className={s.button_wrapper}>
-            <Button>Save Changes</Button>
+            <Button onClick={buttonHandler}>Save Changes</Button>
           </div>
         </div>
       </div>
