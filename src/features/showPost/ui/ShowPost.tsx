6420071ee1e583c91144abe260d086fcbc2017da
Andrew <dracon.24041230@mail.ru>
2025-07-31 23:43:09 +0300

    BAN-71-UC-3 refactor: reconfiguration

--- src/features/showPost/ui/ShowPost.tsx
+++ src/features/showPost/ui/ShowPost.tsx
@@ -2,15 +2,7 @@
 import React, { useState } from 'react'
 import { Button, Popup, Scroll, Typography } from '@shared/ui'
 import { DialogClose, DialogTitle } from '@radix-ui/react-dialog'
-import {
-  BookmarkOutline,
-  Close,
-  Edit2Outline,
-  Heart,
-  HeartOutline,
-  PaperPlaneOutline,
-  TrashOutline,
-} from '@/assets/icons/components'
+import { Close, Edit2Outline, ImageOutline, TrashOutline } from '@/assets/icons/components'
 import s from './ShowPost.module.scss'
 import { VisuallyHidden } from '@radix-ui/react-visually-hidden'
 import Image from 'next/image'
@@ -20,15 +12,15 @@ import { MeatballsMenu } from '@widgets/meatballsMenu/ui/MeatballsMenu'
 import { VerifyModal } from '@features/showPost/components/verifyModal/VerifyModal'
 import type { MeatballsMenuItemData } from '@/widgets'
 import { useGetPostDataQuery } from '@features/showPost/api/api'
-import { EditPostForm } from '@features/edit-post/ui/EditPostForm'
 import { useRouter } from '@/i18n/navigation'
-import Link from 'next/link'
-import Palm from '@/assets/images/Palm.png'
-import type { CommentProps } from '@/features'
+import { Skeleton, SkeletonCircle } from '@shared/ui/skeleton/Skeleton'
+import { SkeletonComment } from '@features/showPost/components/skeletonComment/SkeletonComment'
+import { Comment, SwiperImagesPost } from '@/features'
+import { EngagementInfo } from '@features/showPost/components/engagementInfo/EngagementInfo'
 
 type ShowPostProps = {
-  open: boolean
-  onClose: (value: boolean) => void
+  open?: boolean
+  onClose?: (value: boolean) => void
   id: string
 }
 
@@ -41,17 +33,13 @@ export const ShowPost = (props: ShowPostProps) => {
   const [isOpenVerifyDeleteModal, setOpenVerifyDeleteModal] = useState(false)
   const [isOpenMeatballsMenu, setOpenMeatballsMenu] = useState(false)
   const { data, isFetching } = useGetPostDataQuery(id)
-  const urlImage = data?.items[0].files[0].url
-  const [isEditing, setIsEditing] = useState(false)
-
+  /*const urlImage = data?.items[0].files[0].url*/
+  const urlImages = data?.items[0].files.map(file => file.url)
   const MyPostItems: MeatballsMenuItemData[] = [
     {
       title: 'Edit Post',
       icon: <Edit2Outline />,
-      onClick: () => {
-        setIsEditing(true)
-        setOpenMeatballsMenu(false)
-      },
+      onClick: () => {},
     },
     {
       title: 'Delete Post',
@@ -62,130 +50,136 @@ export const ShowPost = (props: ShowPostProps) => {
       },
     },
   ]
-  const onCloseHandler = () => onClose(false)
-  const handleCloseEditModal = () => setIsEditing(false)
+  const onCloseHandler = () => {
+    onClose?.(false)
+    router.back()
+  }
+  const onClickHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
+    onCloseHandler()
+    e.preventDefault()
+  }
 
   return (
     <>
-      <Popup {...rest} onOpenChange={onClose} size={'xl'}>
-        <DialogClose className={s.close} onClick={onCloseHandler}>
+      <Popup open={true} {...rest} onOpenChange={onCloseHandler} size={'xl'}>
+        <DialogClose className={s.close} onClick={onClickHandler} aria-label="Close post dialog">
           <Close />
         </DialogClose>
         <VisuallyHidden asChild>
           <DialogTitle className={s.hidden_title}>show post</DialogTitle>
         </VisuallyHidden>
         <Scroll>
-          <div className={s.wrapper}>
+          <div className={s.wrapper} aria-busy={isFetching}>
             <div className={s.image_wrapper}>
-              <Image
-                src={data?.items[0].files[0].url || Palm}
-                width={485}
-                height={562}
-                alt={'main-image post'}
-              />
+              {isFetching ? (
+                <Skeleton className={s.skeleton_main_image} aria-label="Loading post content" />
+              ) : urlImages ? (
+                urlImages.length > 0 && <SwiperImagesPost postImages={urlImages} />
+              ) : (
+                <ImageOutline
+                  className={s.io}
+                  viewBox={'0 0 24 24'}
+                  width={'150px'}
+                  height={'150px'}
+                  aria-hidden="true"
+                />
+              )}
             </div>
+
             <div className={s.comments_block}>
               <div className={s.header}>
                 <div className={s.user}>
-                  <CircleImage>
-                    <Image src={user} alt={'user'} />
-                  </CircleImage>
-                  <Typography variant={'h3'}>UserName</Typography>
+                  {isFetching ? (
+                    <SkeletonCircle size={36} aria-label="Loading user avatar" />
+                  ) : (
+                    <CircleImage>
+                      <Image src={user} alt={'User profile picture'} width={36} height={36} />
+                    </CircleImage>
+                  )}
+
+                  {isFetching ? (
+                    <Skeleton width={'100px'} aria-label="Loading username" />
+                  ) : (
+                    <Typography variant={'h3'} id="post-username">
+                      UserName
+                    </Typography>
+                  )}
                 </div>
                 <MeatballsMenu
                   items={MyPostItems}
                   isOpen={isOpenMeatballsMenu}
                   toggleOpen={setOpenMeatballsMenu}
+                  disabled={isFetching}
+                  aria-label="Post options"
                 />
               </div>
-              <Scroll className={s.scroll}>
+
+              <Scroll className={s.scroll} aria-label="Post comments">
                 <div className={s.comments}>
-                  <Comment
-                    text={
-                      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-                    }
-                    title={'UrlProfile'}
-                    image={user}
-                  />
-                  <Comment
-                    text={
-                      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-                    }
-                    title={'UrlProfile'}
-                    image={user}
-                    like={false}
-                  />
-                  <Comment
-                    text={
-                      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-                    }
-                    title={'UrlProfile'}
-                    image={user}
-                    like={true}
-                  />
-                  <Comment
-                    text={
-                      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-                    }
-                    title={'UrlProfile'}
-                    image={user}
-                    like={false}
-                  />
+                  {isFetching ? (
+                    <>
+                      <SkeletonComment />
+                      <SkeletonComment />
+                    </>
+                  ) : (
+                    <>
+                      {' '}
+                      <Comment
+                        text={
+                          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+                        }
+                        title={'UrlProfile'}
+                        image={user}
+                      />
+                      <Comment
+                        text={
+                          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+                        }
+                        title={'UrlProfile'}
+                        image={user}
+                        like={false}
+                      />
+                      <Comment
+                        text={
+                          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+                        }
+                        title={'UrlProfile'}
+                        image={user}
+                        like={true}
+                      />
+                      <Comment
+                        text={
+                          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+                        }
+                        title={'UrlProfile'}
+                        image={user}
+                        like={false}
+                      />{' '}
+                    </>
+                  )}
                 </div>
               </Scroll>
-              <div className={s.engagement_info}>
-                <div className={s.icons_wrapper}>
-                  <div className={s.icons_group}>
-                    <button type={'button'}>
-                      <HeartOutline />
-                    </button>
-                    <button type={'button'}>
-                      <PaperPlaneOutline />
-                    </button>
-                  </div>
-                  <div className={s.icons_group}>
-                    <button type={'button'}>
-                      <BookmarkOutline />
-                    </button>
-                  </div>
-                </div>
-                <div className={s.social_activity}>
-                  <div className={s.last_likes}>
-                    <CircleImage size={'size-24'}>
-                      <Image src={user} alt={'user'} />
-                    </CircleImage>
-                    <CircleImage size={'size-24'}>
-                      <Image src={user} alt={'user'} />
-                    </CircleImage>
-                    <CircleImage size={'size-24'}>
-                      <Image src={user} alt={'user'} />
-                    </CircleImage>
-                  </div>
-                  <span>
-                    <Typography variant={'regular_text_14'} as={'span'}>
-                      <span>2 243 </span>
-                    </Typography>
-                    <Typography variant={'bold_text_14'} as={'span'}>
-                      <span>&quot;Like&quot;</span>
-                    </Typography>
-                  </span>
-                </div>
-                <Typography variant={'small_text'} className={s.date}>
-                  July 3, 2021
-                </Typography>
-              </div>
+              <EngagementInfo postId={id} />
               <div className={s.add_comment}>
-                <div className={s.add_comment_wrapper}>
+                <div className={s.add_comment_wrapper} aria-label="Add a comment">
                   <input
+                    id="comment-input"
                     value={inputValue}
                     onChange={e => {
                       setInputValue(e.currentTarget.value)
                     }}
                     type="text"
                     placeholder={'Add a Comment...'}
+                    disabled={isFetching}
+                    aria-required="true"
                   />
 
-                  <Button variant={'text-button'} type={'button'}>
+                  <Button
+                    disabled={isFetching}
+                    variant={'text-button'}
+                    type={'button'}
+                    aria-label="Publish comment"
+                  >
                     Publish
                   </Button>
                 </div>
@@ -195,69 +189,16 @@ export const ShowPost = (props: ShowPostProps) => {
         </Scroll>
       </Popup>
 
-      {isEditing && <EditPostForm postId={id} open={true} onClose={handleCloseEditModal} />}
-
       <VerifyModal
         title={'Delete Post'}
         isOpenValue={isOpenVerifyDeleteModal}
         onClose={setOpenVerifyDeleteModal}
+        aria-label="Confirm post deletion"
       >
         <Typography variant={'regular_text_16'} className={s.verify_text}>
           Are you sure you want to delete this post?
         </Typography>
       </VerifyModal>
     </>
-  )
-}
-
-const Comment = (props: CommentProps) => {
-  const { like, title, text, image } = props
-  return (
-    <div className={s.comment}>
-      <div className={s.section}>
-        <div className={s.avatar_wrapper}>
-          <CircleImage>
-            <Image src={image} alt={'user'} />
-          </CircleImage>
-        </div>
-        <div className={s.comment_text}>
-          <Typography variant={'regular_text_14'}>
-            <Link href={title} className={s.url}>
-              UrlProfile{' '}
-            </Link>
-            {text}
-          </Typography>
-          <div className={s.comment_information}>
-            <span>
-              <Typography variant={'small_text'}>2 Hours ago</Typography>
-            </span>
-            {like && (
-              <span>
-                <Typography variant={'semi_bold_small_text'}>Like: 1</Typography>
-              </span>
-            )}
-            {like !== undefined && (
-              <span>
-                <Typography variant={'semi_bold_small_text'}>Answer</Typography>
-              </span>
-            )}
-          </div>
-        </div>
-      </div>
-      {like !== undefined && (
-        <div className={s.heart_wrapper}>
-          {!like && (
-            <button type={'button'}>
-              <HeartOutline height={20} width={20} viewBox={'0 0 24 24'} />
-            </button>
-          )}
-          {like && (
-            <button type={'button'}>
-              <Heart className={s.heart} height={20} width={20} viewBox={'0 0 24 24'} />
-            </button>
-          )}
-        </div>
-      )}
-    </div>
   )
 }
