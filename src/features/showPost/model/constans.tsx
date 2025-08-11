20edca45fc552e78dcde30b737cf847724133e78
Andrew <dracon.24041230@mail.ru>
2025-08-11 23:45:05 +0300

    BAN-71-UC-3 refactor: structure

--- src/features/showPost/model/constans.tsx
+++ src/features/showPost/model/constans.tsx
@@ -1,33 +1,29 @@
 import user from '@/assets/images/User.png'
 import type { StaticImageData } from 'next/image'
 
-export type MocComment = { textBody: string; title: string; image: StaticImageData; like?: boolean }
+export type MocComment = { text: string; title: string; image: StaticImageData; like?: boolean }
 
 export const MOC_COMMENTS_DATA: MocComment[] = [
   {
     title: 'UrlProfile',
-    textBody:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
     image: user,
   },
   {
     title: 'UrlProfile',
-    textBody:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
     image: user,
     like: true,
   },
   {
     title: 'UrlProfile',
-    textBody:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
     image: user,
     like: false,
   },
   {
     title: 'UrlProfile',
-    textBody:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
     image: user,
     like: true,
   },
