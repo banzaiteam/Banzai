6d1f9704d72c048db03845041e4acf08ed331894
Andrew <dracon.24041230@mail.ru>
2025-08-12 14:33:17 +0300

    BAN-71-UC-3 refactor:add new endpoint addComment and move types

--- src/features/showPost/api/api.ts
+++ src/features/showPost/api/api.ts
@@ -1,62 +1,33 @@
 import { baseApi } from '@shared/api/baseApi'
-import type { Nullable } from '@shared/types/nullable'
-/*import type { AuthResponse } from '../model/types/types'*/
-type Status = 'pending' | 'success'
-export type CommentPost = {
-  id: string
-  createdAt: Nullable<Date>
-  updatedAt: Nullable<Date>
-  deletedAt: Nullable<Date>
-  userId: string
-  text: string
-  likes: number
-  parentId: Nullable<string>
-}
-type FileData = {
-  id: string
-  createdAt: Date
-  updatedAt: Date
-  fileName: string
-  url: string
-  metatype: string
-  status: Status
-  postId: string
-}
-type PostData = {
-  id: string
-  userId: string
-  isPublished: boolean
-  description: string
-  createdAt: Date
-  updatedAt: Date
-  files: FileData[]
-  comments: CommentPost[] ///🤔
-}
-export type PostDataResponse = {
-  items: PostData[]
-  limit: number
-  page: number
-  totalItems: number
-}
-type DeletePostResponse = {}
+import type { AddCommentResponse, DeletePostResponse, PostDataResponse } from '@/features'
+
 export const showPostApi = baseApi.injectEndpoints({
   endpoints: build => ({
-    deletePost: build.mutation<DeletePostResponse, string>({
-      query: id => ({
-        url: `/posts/${id}`,
-        method: 'DELETE',
-      }),
-    }),
     getPostData: build.query<PostDataResponse, string>({
-      query: postid => ({
+      query: postId => ({
         url: `/posts`,
         method: 'GET',
         params: {
-          filter: `id:eq:${postid}`,
+          filter: `id:eq:${postId}`,
         },
       }),
+      providesTags: (result, error, postId) => [{ type: 'Post', id: postId }],
+    }),
+    deletePost: build.mutation<DeletePostResponse, string>({
+      query: postId => ({
+        url: `/posts/${postId}`,
+        method: 'DELETE',
+      }),
+    }),
+    addComment: build.mutation<AddCommentResponse, { postId: string; text: string }>({
+      query: body => ({
+        url: `/posts/comments`,
+        method: 'POST',
+        body,
+      }),
+      invalidatesTags: (result, error, arg) => [{ type: 'Post', id: arg.postId }],
     }),
   }),
 })
 
-export const { useDeletePostMutation, useGetPostDataQuery } = showPostApi
+export const { useDeletePostMutation, useGetPostDataQuery, useAddCommentMutation } = showPostApi
