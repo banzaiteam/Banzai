badcfe50e8323199c91c761583eb16572c1e5ca0
Andrew <dracon.24041230@mail.ru>
2025-09-05 16:22:55 +0300

    BAN-95 fix: convert files into formData

--- src/features/createPost/api/api.ts
+++ src/features/createPost/api/api.ts
@@ -4,13 +4,19 @@ import type { CreatePostResponse } from '@features/createPost/model/types'
 export const createPostApi = baseApi.injectEndpoints({
   endpoints: build => ({
     createPost: build.mutation<CreatePostResponse, { files: File[] }>({
-      query: ({ files }) => ({
-        url: '/posts',
-        method: 'POST',
-        body: {
-          files,
-        },
-      }),
+      query: ({ files }) => {
+        const formData = new FormData()
+
+        files.forEach(file => {
+          formData.append('files', file) // 'files' - имя поля, которое ожидает сервер
+        })
+
+        return {
+          url: '/posts',
+          method: 'POST',
+          body: formData,
+        }
+      },
     }),
   }),
 })
