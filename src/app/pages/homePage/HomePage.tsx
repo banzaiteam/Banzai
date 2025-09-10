68f87569b1b95015fc24d80acff14222bbced7b6
Andrew <dracon.24041230@mail.ru>
2025-09-10 12:21:27 +0300

    BAN-100 refactor: add counter user

--- src/app/pages/homePage/HomePage.tsx
+++ src/app/pages/homePage/HomePage.tsx
@@ -1,22 +1,30 @@
-'use client'
-import s from './HomePage.module.scss'
 import { CounterRegUsers } from '@/app/pages'
 import { LastPosts } from '@/app/pages/homePage/components/lastPosts/LastPosts'
-import { useGetMeQuery } from '@shared/api/userApi'
-import clsx from 'clsx'
+import s from './HomePage.module.scss'
+import { WrapperPage } from './WrapperPage'
 
-export const HomePage = () => {
-  const { data } = useGetMeQuery()
+type ResponseData = {
+  usersAmount: number
+}
 
-  const isNotAuth = !data
-  const styles = clsx(s.wrapper, {
-    [s.not_auth]: isNotAuth,
-  })
+// Перевалидировать каждые 60 секунд
 
+/*export async function generateStaticParams() {
+  const data: ResponseData = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/posts/main`).then(
+    res => res.json()
+  )
+  return data
+}*/
+
+export const HomePage = async () => {
+  const data: ResponseData = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/posts/main`, {
+    next: { revalidate: 60 },
+  }).then(res => res.json())
+  console.log(data)
   return (
-    <div className={styles}>
-      <CounterRegUsers className={s.counter_reg_users} />
+    <WrapperPage>
+      <CounterRegUsers usersAmount={data.usersAmount} className={s.counter_reg_users} />
       <LastPosts />
-    </div>
+    </WrapperPage>
   )
 }
--- src/app/pages/homePage/components/counterRegUsers/CounterRegUsers.tsx
+++ src/app/pages/homePage/components/counterRegUsers/CounterRegUsers.tsx
@@ -1,25 +1,33 @@
-import React, { type ComponentPropsWithoutRef } from 'react'
+import React, { type ComponentPropsWithoutRef, useMemo } from 'react'
 import s from './CounterRegUsers.module.scss'
 import { Card } from '@shared/ui'
 import clsx from 'clsx'
+import { useTranslations } from 'next-intl'
 
-type Props = Omit<ComponentPropsWithoutRef<'div'>, 'children'>
+const DIGITS_COUNT = 6
+
+type Props = Omit<ComponentPropsWithoutRef<'div'>, 'children'> & {
+  usersAmount: number
+}
 
 export const CounterRegUsers = (props: Props) => {
-  const { className, ...rest } = props
+  const { className, usersAmount, ...rest } = props
   const styles = clsx(s.counter_wrapper, className)
+  const t = useTranslations('CounterRegUsers')
+
+  /*const arrayNumbers = useMemo(() => usersAmount.toString().split(''), [usersAmount])*/
+
+  const countUsers = useMemo(() => {
+    const count = usersAmount.toString().padStart(DIGITS_COUNT, '0').split('')
+
+    return count.map((num, index) => <li key={index}>{num}</li>)
+  }, [usersAmount])
+
   return (
     <div className={styles} {...rest}>
-      <span className={s.counter_text}>Registered users:</span>
+      <span className={s.counter_text}>{t('RegUsers')}</span>
       <Card className={s.counter}>
-        <ul className={s.counter__list}>
-          <li>0</li>
-          <li>0</li>
-          <li>9</li>
-          <li>2</li>
-          <li>1</li>
-          <li>3</li>
-        </ul>
+        <ul className={s.counter__list}>{countUsers}</ul>
       </Card>
     </div>
   )
