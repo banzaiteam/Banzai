c8a36599dd29df3812392f5e9bd53f872559c752
Andrew <dracon.24041230@mail.ru>
2025-09-12 18:29:05 +0300

    BAN-100 refactor: add animation

--- src/app/pages/homePage/components/counterRegUsers/CounterRegUsers.tsx
+++ src/app/pages/homePage/components/counterRegUsers/CounterRegUsers.tsx
@@ -1,9 +1,11 @@
-import React, { type ComponentPropsWithoutRef, useMemo } from 'react'
+'use client'
+import React, { type ComponentPropsWithoutRef, useEffect, useMemo } from 'react'
 import s from './CounterRegUsers.module.scss'
 import { Card } from '@shared/ui'
 import clsx from 'clsx'
 import { useTranslations } from 'next-intl'
-
+import { animate, useMotionValue, useTransform } from 'framer-motion'
+import { motion } from 'framer-motion'
 const DIGITS_COUNT = 6
 
 type Props = Omit<ComponentPropsWithoutRef<'div'>, 'children'> & {
@@ -20,7 +22,7 @@ export const CounterRegUsers = (props: Props) => {
   const countUsers = useMemo(() => {
     const count = (usersAmount || 0).toString().padStart(DIGITS_COUNT, '0').split('')
 
-    return count.map((num, index) => <li key={index}>{num}</li>)
+    return count.map((num, index) => <CountNumber key={index} num={+num} />)
   }, [usersAmount])
 
   return (
@@ -30,5 +32,19 @@ export const CounterRegUsers = (props: Props) => {
         <ul className={s.counter__list}>{countUsers}</ul>
       </Card>
     </div>
+  )
+}
+
+const CountNumber = ({ num }: { num: number }) => {
+  const count = useMotionValue(0)
+  const rounded = useTransform(() => Math.round(count.get()))
+  useEffect(() => {
+    const controls = animate(count, num, { duration: 4 })
+    return () => controls.stop()
+  }, [])
+  return (
+    <li>
+      <motion.pre>{rounded}</motion.pre>
+    </li>
   )
 }
