1a63a37b8e87ba6fba3aea8c8b38c38456f2f6e2
VitaliPri <vitaliy.prymak@cix.csi.cuny.edu>
2025-10-09 21:32:22 -0400

    feat: BAN-115-TypeOfAccount

--- src/widgets/profileSettings/components/accountManagement/AccountManagement.tsx
+++ src/widgets/profileSettings/components/accountManagement/AccountManagement.tsx
@@ -1,3 +1,27 @@
+import { RadioGroup } from '@/shared/ui/radioGroup/RadioGroup'
+import styles from './AccountManagement.module.scss'
+import { useState } from 'react'
+
 export const AccountManagement = () => {
-  return <div>AccountManagement</div>
+  const [accountType, setAccountType] = useState('personal')
+
+  return (
+    <section>
+      <div className={styles.container}>
+        <div className={styles.container__inner}>
+          <p className={styles.account}>Account type:</p>
+          <div className={styles.type}>
+            <RadioGroup
+              options={[
+                { label: 'Personal', value: 'personal' },
+                { label: 'Business', value: 'business' },
+              ]}
+              value={accountType}
+              onValueChange={val => setAccountType(val)}
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
